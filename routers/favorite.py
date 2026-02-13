e996283337514ae82b2147ae39f69733e693bafd
Noctics <18401453152@139.com>
2026-02-13 18:24:27 +0800

    10

--- routers/favorite.py
+++ routers/favorite.py
@@ -6,7 +6,7 @@ from models.users import User
 from utils.auth import get_current_user
 from utils.response import success_response
 from crud import favorite
-from schemas.favorite import FavoriteCheckResponse
+from schemas.favorite import FavoriteCheckResponse,FavoriteAddRequest
 
 
 router=APIRouter(prefix="/api/favorite",tags=["favorite"])
@@ -19,3 +19,19 @@ async def check_favorite(
 ):
     is_favorite=await favorite.is_news_favorite(db,user.id,news_id)
     return success_response(message="检查收藏状态成功",data=FavoriteCheckResponse(isFavorite=is_favorite))
+
+
+@router.post("/add")
+async def add_favorite(
+    data: FavoriteAddRequest,
+    user: User = Depends(get_current_user),
+    db: AsyncSession = Depends(get_db)
+):
+    result=await favorite.add_news_favorite(db,user.id,data.news_id)
+    return success_response(message="添加收藏成功",data=result)
+
+
+
+
+
+
