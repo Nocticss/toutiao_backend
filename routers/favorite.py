3df6a35dab569c703e57a9459621b9d83bf1ca77
Noctics <18401453152@139.com>
2026-02-14 17:32:45 +0800

    12

--- routers/favorite.py
+++ routers/favorite.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Query,Depends
+from fastapi import APIRouter, HTTPException,Query,Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from config.db_conf import get_db
@@ -6,7 +6,7 @@ from models.users import User
 from utils.auth import get_current_user
 from utils.response import success_response
 from crud import favorite
-from schemas.favorite import FavoriteCheckResponse,FavoriteAddRequest
+from schemas.favorite import FavoriteCheckResponse,FavoriteAddRequest,FavoriteListResponse
 
 
 router=APIRouter(prefix="/api/favorite",tags=["favorite"])
@@ -41,6 +41,28 @@ async def remove_favorite(
     if not result:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="收藏记录不存在")
     return success_response(message="删除收藏成功")
+
+
+
+@router.get("/list")
+async def get_favorite_list(
+    page:int=Query(1,ge=1),
+    page_size:int=Query(10,ge=1,le=100,alias="pageSize"),
+    user:User=Depends(get_current_user),
+    db:AsyncSession=Depends(get_db)
+):
+    rows,total=await favorite.get_favorite_list(db,user.id,page,page_size)
+    favorite_list = [{
+        **news.__dict__,
+        "favorite_time": favorite_time,
+        "favorite_id": favorite_id
+    } for news, favorite_time, favorite_id in rows]
+    has_more = total > page * page_size
+
+    data = FavoriteListResponse(list=favorite_list, total=total, hasMore=has_more)
+    return success_response(message="获取收藏列表成功", data=data)
+
+
 
 
 
--- schemas/favorite.py
+++ schemas/favorite.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel,Field
+from pydantic import BaseModel,Field,ConfigDict
+from datetime import datetime
+from schemas.base import NewsItemBase
 
 class FavoriteCheckResponse(BaseModel):
     is_favorite:bool=Field(...,alias="isFavorite")
@@ -6,3 +8,25 @@ class FavoriteCheckResponse(BaseModel):
 
 class FavoriteAddRequest(BaseModel):
     news_id:int=Field(...,alias="newsId")
+
+#规划两个类：一个是新闻模型类+收藏的模型类
+class FavoriteNewsItemResponse(NewsItemBase):
+    favorite_id:int=Field(alias="favoriteId")
+    favorite_time:datetime=Field(alias="favoriteTime")
+
+    model_config=ConfigDict(
+        populate_by_name=True,
+        from_attributes=True
+    )
+
+
+#收藏列表接口响应模型类
+class FavoriteListResponse(BaseModel):
+    list:list[FavoriteNewsItemResponse]
+    total:int
+    has_more:bool=Field(alias="hasMore")
+
+    model_config=ConfigDict(
+        populate_by_name=True,
+        from_attributes=True
+    )
